4306aeb96637f421cbce462e9efeed64f08e3226
Ahrayz <Ahrayz@users.noreply.github.com>
2018-04-05 01:16:21 +0800

    on trans_2 120 first rows

--- last_is_churn.py
+++ last_is_churn.py
@@ -12,15 +12,17 @@ import pandas as pd
 import datetime
 from tqdm import tqdm
 
-#--------------------------
-transactions_v1 = pd.read_csv('transactions.csv')
-transactions_v2 = pd.read_csv('transactions_v2.csv')
-transactions = transactions_v1.append(transactions_v2).reset_index(drop=True)
-transactions = transaction.drop(columns=['payment_method_id', 'payment_plan_days',
-                                         'plan_list_price', 'actual_amount_paid', 'is_auto_renew', 'is_cancel'])
+transactions = pd.read_csv('trans_test.csv').reset_index(drop=True)
+#transactions_v1 = pd.read_csv('transactions.csv')
+#transactions_v2 = pd.read_csv('transactions_v2.csv')
+#transactions = transactions_v1.append(transactions_v2).reset_index(drop=True)
+
+transactions = transactions.drop(columns=['payment_method_id', 'payment_plan_days',
+                                          'plan_list_price', 'actual_amount_paid', 'is_auto_renew', 'is_cancel'])
 transactions = transactions.sort_values(
     ['msno', 'transaction_date'])
-result = pd.DataFrame(columns=['msno','last_1_is_churn','last_2_is_churn','last_3_is_churn','last_4_is_churn','last_5_is_churn','churn_rate','churn_count'])
+# result = pd.DataFrame(columns=['msno','last_1_is_churn','last_2_is_churn','last_3_is_churn','last_4_is_churn','last_5_is_churn','churn_rate','churn_count'])
+result = pd.DataFrame()
 
 
 transaction_dates = []
@@ -28,24 +30,8 @@ membership_expire_dates = []
 prev_msno = ''
 total_rows = len(transactions['msno'])
 
-for i, row in tqdm(transactions.iterrows(), total=total_rows):
-    msno = row['msno']
-    transaction_date = row['transaction_date']
-    membership_expire_date = row['membership_expire_date']
 
-    if (msno != prev_msno):
-        calc_churn(transaction_dates, membership_expire_dates, prev_msno)
-        transaction_dates = []
-        membership_expire_dates = []  # clear lists, start recording this new user id
-
-    transaction_dates.append(transaction_date)
-    membership_expire_dates.append(membership_expire_date)
-    prev_msno = msno
-
-result.to_csv('last_is_churns.csv'.format(target), index=False)
-
-
-def calc_churn(t_dates, e_dates, msno):
+def calc_churn(t_dates, e_dates, msno, result):
     churns = []
     for i, e_date in enumerate(e_dates):
         if int(e_date) / 100 >= 201703:  # if expiration data is 201703 onwards, treat as no churn
@@ -65,9 +51,31 @@ def calc_churn(t_dates, e_dates, msno):
         churn_rate = (sum(churns) / len(churns))
     churn_count = sum(churns)
 
-    while len(churns) <= 5:
+    while len(churns) < 5:
         churns.append(0)
+
     df = {'msno': [msno], 'last_1_is_churn': [churns[0]], 'last_2_is_churn': [churns[1]], 'last_3_is_churn': [
         churns[2]], 'last_4_is_churn': [churns[3]], 'last_5_is_churn': [churns[4]], 'churn_rate': [churn_rate], 'churn_count': [churn_count]}
     #df = pd.DataFrame(data=np.array([[1, 2, 3]]), columns=['msno','last_1_is_churn','last_2_is_churn','last_3_is_churn','last_4_is_churn','last_5_is_churn'])
-    result.append(pd.DataFrame(data=df))
+    return result.append(pd.DataFrame(data=df))
+    # print(pd.DataFrame(data=df))
+
+
+for i, row in tqdm(transactions.iterrows(), total=total_rows):
+    msno = row['msno']
+    transaction_date = row['transaction_date']
+    membership_expire_date = row['membership_expire_date']
+
+    if (msno != prev_msno):
+        result = calc_churn(transaction_dates,
+                            membership_expire_dates, prev_msno, result)
+        transaction_dates = []
+        membership_expire_dates = []  # clear lists, start recording this new user id
+
+    transaction_dates.append(transaction_date)
+    membership_expire_dates.append(membership_expire_date)
+    prev_msno = msno
+
+result = result[['msno', 'last_1_is_churn',
+                 'last_2_is_churn', 'last_3_is_churn', 'last_4_is_churn', 'last_5_is_churn', 'churn_rate', 'churn_count']]
+result.to_csv('last_is_churns.csv', index=False)
