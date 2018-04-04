22261c130379d8bb3d7910be2cedd39e4757b3eb
Ahrayz <Ahrayz@users.noreply.github.com>
2018-04-05 00:33:52 +0800

    initialize churn_rate

--- last_is_churn.py
+++ last_is_churn.py
@@ -20,7 +20,8 @@ transactions = transaction.drop(columns=['payment_method_id', 'payment_plan_days
                                          'plan_list_price', 'actual_amount_paid', 'is_auto_renew', 'is_cancel'])
 transactions = transactions.sort_values(
     ['msno', 'transaction_date'])
-result=dp.DataFrame(columns=['msno','last_1_is_churn','last_2_is_churn','last_3_is_churn','last_4_is_churn','last_5_is_churn','churn_rate','churn_count'])
+result = dp.DataFrame(columns=['msno', 'last_1_is_churn', 'last_2_is_churn',
+                               'last_3_is_churn', 'last_4_is_churn', 'last_5_is_churn', 'churn_rate', 'churn_count'])
 
 transaction_dates = []
 membership_expire_dates = []
@@ -43,27 +44,30 @@ for i, row in tqdm(transactions.iterrows(), total=total_rows):
 
 result.to_csv('last_is_churns.csv'.format(target), index=False)
 
+
 def calc_churn(t_dates, e_dates, msno):
     churns = []
     for i, e_date in enumerate(e_dates):
-        if int(e_date)/100>=201703:# if expiration data is 201703 onwards, treat as no churn
-            churns.insert(0,0)
+        if int(e_date) / 100 >= 201703:  # if expiration data is 201703 onwards, treat as no churn
+            churns.insert(0, 0)
             continue
         expired_date = datetime.datetime.strptime(str(e_date), "%Y%m%d")
         churn = 1
         for t_date in enumerate(t_dates):
             trans_date = datetime.datetime.strptime(str(t_date), "%Y%m%d")
             dif_d = (trans_date - expired_date).days
-            if (0 <= dif_d < 30): # if some trans resubscribe
+            if (0 <= dif_d < 30):  # if some trans resubscribe
                 churn = 0
-        churns.insert(0,churn)
+        churns.insert(0, churn)
 
-    if len(churns)>0:
-        churn_rate=(sum(churns) / len(churns))
-    churn_count=sum(churns)
+    churn_rate = 0
+    if len(churns) > 0:
+        churn_rate = (sum(churns) / len(churns))
+    churn_count = sum(churns)
 
-    while len(churns)<=5:
+    while len(churns) <= 5:
         churns.append(0)
-    df = {'msno':[msno],'last_1_is_churn':[churns[0]],'last_2_is_churn':[churns[1]],'last_3_is_churn':[churns[2]],'last_4_is_churn':[churns[3]],'last_5_is_churn': [churns[4]],'churn_rate':[churn_rate],'churn_count':[churn_count]}
+    df = {'msno': [msno], 'last_1_is_churn': [churns[0]], 'last_2_is_churn': [churns[1]], 'last_3_is_churn': [
+        churns[2]], 'last_4_is_churn': [churns[3]], 'last_5_is_churn': [churns[4]], 'churn_rate': [churn_rate], 'churn_count': [churn_count]}
     #df = pd.DataFrame(data=np.array([[1, 2, 3]]), columns=['msno','last_1_is_churn','last_2_is_churn','last_3_is_churn','last_4_is_churn','last_5_is_churn'])
     result.append(pd.DataFrame(data=df))
